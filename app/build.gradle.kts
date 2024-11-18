c004aed2da9df74d9d2065f6ee6b559f1d3c17d7
coalstar <y.liang1994@gmail.com>
2024-11-17 19:06:16 -0800

    Added Lab 4 content to project and changed existing login/register pages to be more in line with lab 4
    Added internet permission to AndroidManifest
    Added view binding to project
    Added RoundedImageView
    Integrated Google Firebase project with app, added Auth, Messaging, and Firestore
    Revised Login page, added login functionality using Google Firebase
    Added Register page, added register functionality using Google Firebase
    Started on PreferenceManager and loading icon for login/register page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -26,14 +27,20 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
+    implementation(libs.roundedimageview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
